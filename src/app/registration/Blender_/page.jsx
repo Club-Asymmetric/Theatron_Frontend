9ddb95bcd60d174a86312bbf44f24a9e72013438
Lokith007 <lokithk93@gmail.com>
2025-11-04 20:50:21 +0530

    testing

--- src/app/registration/Blender_/page.jsx
+++ src/app/registration/Blender_/page.jsx
@@ -22,7 +22,7 @@ export default function BlenderWorkshop() {
         phone: formData.phone,
         email: formData.email,
         college: formData.college,
-        amount: 150,
+        amount: 1,
         currency: "INR",
         receipt: `blender_${Date.now()}`,
         redirect: "https://theatron-nu.vercel.app/success"
